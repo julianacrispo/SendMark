336156d8cf0672133e7ce74c3774ad9cd6dc8dce
Juliana Crispo <julianacrispo@gmail.com>
2014-07-03 14:46:05 -0400

    configured mailgun

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,15 @@
+class UserMailer < ActionMailer::Base
+  default from: "julianacrispo@gmail.com"
+
+def 
+
+#test
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  
+
   get 'topics/index'
 
   get 'bookmarks/index'
+
+  post :incoming, to: 'incoming#create'
+  get 'incoming/index'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
