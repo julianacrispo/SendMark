3463f581e58a4c10c2a30262e9b5627943b82a31
Juliana Crispo <julianacrispo@gmail.com>
2014-07-14 16:04:29 -0400

    adjusted incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -9,17 +9,8 @@ class IncomingController < ApplicationController
     end
 
     def create
-    # Take a look at these in your server logs
-    # to get a sense of what you're dealing with.
     logger.info "!!!!!!!!!!!!!!!!"
     logger.info "INCOMING PARAMS HERE: #{params}"
-
-    # get topic based on subject
-      # find existing topic
-      # else create new one
-
-    # create bookmark based on body
-    # assign topic and current_user to bookmark
 
     user = User.find_by_email params[:sender]
 
@@ -33,33 +24,28 @@ class IncomingController < ApplicationController
     @bookmark.topic = topic
     @bookmark.user = user
     @bookmark.save
-
-    # You put the message-splitting and business
-    # magic here. 
-
-    # Assuming all went well. 
     head 200
   end
 
-  def post
-     # process various message parameters:
-     sender  = params['from']
-     subject = params['subject']
+#   def post
+#      # process various message parameters:
+#      sender  = params['from']
+#      subject = params['subject']
 
-     # get the "stripped" body of the message, i.e. without
-     # the quoted part
-     actual_body = params["stripped-text"]
+#      # get the "stripped" body of the message, i.e. without
+#      # the quoted part
+#      actual_body = params["stripped-text"]
 
-     # process all attachments:
-     count = params['attachment-count'].to_i
-     count.times do |i|
-       stream = params["attachment-#{i+1}"]
-       filename = stream.original_filename
-       data = stream.read()
-     end
-     render :text => "OK"
-   end
-end
+#      # process all attachments:
+#      count = params['attachment-count'].to_i
+#      count.times do |i|
+#        stream = params["attachment-#{i+1}"]
+#        filename = stream.original_filename
+#        data = stream.read()
+#      end
+#      render :text => "OK"
+#    end
+# end
 
 #   def index 
 #     @topic = Topic.new
