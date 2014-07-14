f0c9fc46974e66270e31b08ff2782543502dfe47
Juliana Crispo <julianacrispo@gmail.com>
2014-07-14 18:54:58 -0400

    fixed mailer

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   address:           'smtp.mailgun.org',
   user_name:         ENV['MAILGUN_SMTP_LOGIN'],
   password:          ENV['MAILGUN_SMTP_PASSWORD'],
-  domain:            'app26886204.mailgun.org',
+  domain:            'sendmarkproject.herokuapp.com', 
   authentication:    :plain,
   content_type:      'text/html'
 }
